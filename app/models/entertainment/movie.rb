363ece98a47313bf6b17713deb6a3c266927c1cf
Dan Cunning <dancunning@gmail.com>
2015-08-28 15:20:11 -0400

    Internal Updates

--- app/models/entertainment/movie.rb
+++ app/models/entertainment/movie.rb
@@ -6,46 +6,9 @@ module Entertainment
       t.string :title
       t.integer :year
       t.date :release_date
-      t.string :netflix_url
-      t.string :hbogo_url
-      t.string :maxgo_url
-      t.string :showtime_url
-      t.string :apple_trailers_url
-      t.string :redbox_url
       t.string :poster_url
-      t.string :imdb_url
-      t.string :rotten_tomatoes_url
-      t.string :amazon_url
-      t.boolean :on_netflix_instant
-      t.boolean :on_amazon_prime
-    end
-
-    def rotten_tomatoes?
-      !rotten_tomatoes_url.blank?
-    end
-
-    def netflix_instant?
-      on_netflix_instant?
-    end
-
-    def hbogo?
-      !hbogo_url.blank?
-    end
-
-    def maxgo?
-      !maxgo_url.blank?
-    end
-
-    def showtime?
-      !showtime_url.blank?
-    end
-
-    def amazon?
-      !amazon_url.blank?
-    end
-
-    def amazon_prime?
-      on_amazon_prime?
+      t.hash :streams
+      t.hash :references
     end
 
   end
--- app/models/entertainment/tv_show.rb
+++ app/models/entertainment/tv_show.rb
@@ -4,47 +4,13 @@ module Entertainment
 
     columns do |t|
       t.string :title
-      t.string :network
-      t.string :imdb_url
-      t.string :wikipedia_url
-      t.string :hulu_url
-      t.string :hbogo_url
-      t.string :maxgo_url
-      t.string :showtime_url
-      t.string :netflix_url
-      t.string :amazon_url
       t.string :poster_url
-      t.boolean :on_netflix_instant
-      t.boolean :on_amazon_prime
       t.date :latest_episode_aired_on
-    end
-
-    def netflix_instant?
-      on_netflix_instant?
-    end
-
-    def hulu?
-      !hulu_url.blank?
-    end
-
-    def hbogo?
-      !hbogo_url.blank?
-    end
-
-    def maxgo?
-      !maxgo_url.blank?
-    end
-
-    def showtime?
-      !showtime_url.blank?
-    end
-
-    def amazon?
-      !amazon_url.blank?
-    end
-
-    def amazon_prime?
-      on_amazon_prime?
+      t.date :first_episode_aired_on
+      t.date :final_episode_aired_on
+      t.integer :unwatched_episodes_count
+      t.hash :streams
+      t.hash :references
     end
 
   end
