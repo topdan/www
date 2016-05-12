e927824be6ce69202bc61d09cb4e0fbfe3e9a657
Dan Cunning <dancunning@gmail.com>
2016-05-12 17:59:39 -0400

    Internal Updates

---
+++ assets/static/ruby-on-rails/ajax-toggle-buttons/1/favorite/delete-b8a19cd8d59466621c0b1579fb7807a8.js
@@ -0,0 +1,1 @@
+$("#post-1 .favorite").replaceWith('<a href="/assets/static/ruby-on-rails/ajax-toggle-buttons/1/favorite/put-ec073936b0a9a95d34278ecf77b2696a.js" data-method="GET" data-remote="true" data-type="script" class="btn btn-default favorite"> <span class="fa fa-star">&nbsp;</span> Favorite</a>');
